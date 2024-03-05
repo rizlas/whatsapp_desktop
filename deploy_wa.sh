978b500388ea4ae9aacabf4dffb64653b521508e
rizlas <fonzy9114@gmail.com>
2024-03-05 14:28:02 +0100

    use docker compose v2

--- deploy_wa.sh
+++ deploy_wa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose up && chmod 755 -R apps/whatsApp-linux-x64 && rm -rf /opt/whatsApp && mv apps/whatsApp-linux-x64 /opt/whatsApp
+docker compose up && chmod 755 -R apps/whatsApp-linux-x64 && rm -rf /opt/whatsApp && mv apps/whatsApp-linux-x64 /opt/whatsApp
